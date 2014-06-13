6f05a3d50ca23ae4684d9e4c9b4bb37ef736dc1d
Mike Whiting <mail@mikejw.co.uk>
2014-06-13 22:29:21 +0100

    fixes.

--- src/Empathy/ELib/Model.php
+++ src/Empathy/ELib/Model.php
@@ -9,24 +9,27 @@ class Model extends EmpModel
     private static $elib_model_prefix = 'Empathy\ELib\Storage\\';
     private static $app_model_prefix = 'Empathy\MVC\Model\\';
 
+
+    private static function getClass($model)
+    {
+        $file = $model.'.php';
+        $app_file = DOC_ROOT.'/storage/'.$file;
+
+        if (!file_exists($app_file)) {
+            $class = self::$elib_model_prefix.$model;
+        } else {
+            require_once($app_file);
+            $class = self::$app_model_prefix.$model;
+        }
+        return $class;
+    }
+
     
     public static function load($model, $id=null, $params=array(), $host=null)
     {
         $storage_object = null;
-        $file = $model.'.php';
-        $app_file = DOC_ROOT.'/storage/'.$file;
-
         
-        if(!file_exists($app_file)) {
-            $class = self::$elib_model_prefix.$model;
-        } else {
-
-            require_once($app_file);
-            $class = self::$app_model_prefix.$model;
-        }
-
-        $reflect  = new \ReflectionClass($class);
-
+        $reflect  = new \ReflectionClass(self::getClass($model));
 
         if(sizeof($params)) {
             $storage_object = $reflect->newInstanceArgs($params);
@@ -42,18 +45,10 @@ class Model extends EmpModel
         return $storage_object;
     }
 
+
     public static function getTable($model)
     {
-        $file = $model.'.php';
-        $app_file = DOC_ROOT.'/storage/'.$file;
-
-        if (!file_exists($app_file)) {
-            $class = self::$elib_model_prefix.$model;
-        } else {
-            $class = self::$app_model_prefix.$model;
-        }
-
+        $class = self::getClass($model); 
         return $class::TABLE;
     }
-
 }
