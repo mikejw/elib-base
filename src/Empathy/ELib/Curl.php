0952b9dd601d8b1500f1d4e10c247c3f6e930cc8
Mike Whiting <mail@mikejw.co.uk>
2014-09-16 11:03:27 +0100

    small improvements.

--- src/Empathy/ELib/Curl.php
+++ src/Empathy/ELib/Curl.php
@@ -37,6 +37,8 @@ class Curl
         curl_setopt($this->ch, CURLOPT_URL, $this->url);
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
 
+
+
         //    curl_setopt($this->ch, CURLOPT_HTTPHEADER, array());
 
         //    curl_setopt($this->ch, CURLOPT_USERPWD, $auth);
@@ -52,11 +54,11 @@ class Curl
     public function fetch($disconnect=true)
     {
         $this->response = curl_exec($this->ch);
-        $this->success = (curl_getinfo($this->ch, CURLINFO_HTTP_CODE) == 200);
+        $code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+        $this->success = ($code == 200);
         if($disconnect === true) {
             curl_close($this->ch);
         }
-
         return $this->success;
     }
 
