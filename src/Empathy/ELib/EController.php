6788410f6def690cac363db0ef958ab9d52da4d4
Mike Whiting <mail@mikejw.co.uk>
2015-07-10 17:40:50 +0100

    fixes.

--- src/Empathy/ELib/EController.php
+++ src/Empathy/ELib/EController.php
@@ -8,17 +8,18 @@ use Empathy\MVC\Controller\CustomController;
 
 class EController extends CustomController
 {
+    protected $elib_tpl_dirs;
 
     public function __construct($boot)
     {
         parent::__construct($boot);
 
         CurrentUser::detectUser($this);
-        $elib_tpl_dirs = Util\Libs::detetct();
-        if (sizeof($elib_tpl_dirs) > 1) {
-            $this->assign('elibtpl_arr', $elib_tpl_dirs);
+        $this->elib_tpl_dirs = Util\Libs::detect();
+        if (sizeof($this->elib_tpl_dirs) > 1) {
+            $this->assign('elibtpl_arr', $this->elib_tpl_dirs);
         } else {
-            $this->assign('elibtpl', $elib_tpl_dirs[0]);
+            $this->assign('elibtpl', $this->elib_tpl_dirs[0]);
         }
         if (Util\Libs::getStoreActive()) {
             CurrentUser::detectUser($this, true);
