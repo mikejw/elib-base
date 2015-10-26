f9ff3572d919e7a285c403023c380c5d4dc402ed
Mike Whiting <mail@mikejw.co.uk>
2015-10-26 15:49:35 +0000

    adding configure post.

--- src/Empathy/ELib/REST.php
+++ src/Empathy/ELib/REST.php
@@ -7,16 +7,20 @@ class REST extends Curl
 
     public function configure()
     {
-
         if ($this->auth) {
             $auth_string = $this->user.':'.$this->pass;
             curl_setopt($this->ch, CURLOPT_USERPWD, $auth_string);
         }
 
         curl_setopt($this->ch, CURLOPT_POST, 0);
-
         curl_setopt($this->ch, CURLOPT_URL, $this->url);
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    }
+
+    public function configure_post()
+    {
+        curl_setopt($this->ch, CURLOPT_POST, 1);
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($this->post_fields));
     }
 
 }
