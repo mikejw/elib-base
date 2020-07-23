da0260896be79e0e71e64f505c3d8893f4c10092
Mike Whiting <mikejw3@gmail.com>
2020-07-23 12:25:39 +0100

    refactor to service and small sql fix

--- src/Empathy/ELib/Contact/FrontController.php
+++ src/Empathy/ELib/Contact/FrontController.php
@@ -6,62 +6,35 @@ use Empathy\ELib\EController;
 use Empathy\ELib\Mailer;
 use Empathy\ELib\Model;
 use Empathy\MVC\Entity;
+use Empathy\MVC\DI;
 
 class FrontController extends EController
 {
 
     public function default_event()
     {
-        $c = Model::load('Contact');
+        $service =  DI::getContainer()->get('Contact');
         $this->initID('id', 1, true);
-
         if (isset($_POST['submit'])) {
-            $c->assignFromPost(array('submitted', 'message', 'subject', 'body'));
-            $c->message = 0;
-            $c->submitted = 'MYSQLTIME';
-
-            $c->validates();
-            if ($c->hasValErrors()) {
-                $this->presenter->assign('errors', $c->getValErrors());
+            if (!$service->signUp()) {
+                $this->presenter->assign('errors', $service->getErrors());
             } else {
-                $c->id = $c->insert(Model::getTable('Contact'), true, array(''), Entity::SANITIZE);
+                $service->persist();
                 $this->redirect('contact/thanks/1');
             }
 
         } elseif (isset($_POST['submit_msg'])) {
-            $c->assignFromPost(array('message', 'submitted'));
-            $c->message = 1;
-            $c->submitted = 'MYSQLTIME';
 
-            $c->validates();
-            if ($c->hasValErrors()) {
-                $this->presenter->assign('errors', $c->getValErrors());
+            if (!$service->email()) {
+                $this->presenter->assign('errors', $service->getErrors());
             } else {
-                // send email
-                $r = array();
-                if (
-                    Config::get('EMAIL_ORGANISATION') &&
-                    Config::get('EMAIL_FROM') &&
-                    Config::get('EMAIL_RECIPIENT')
-                ) {
-                    $r[0]['alias'] = Config::get('EMAIL_RECIPIENT');
-                    $r[0]['address'] = Config::get('EMAIL_RECIPIENT');
-
-                    $messageOut = "Message sent from: "
-                        .$c->first_name. ' ' .$c->last_name
-                        ." - ".$c->email."<br /><br />"
-                        .$c->body;
-
-                    $m = new Mailer($r, $c->subject, $messageOut, null, true);
-                }
-
-                $c->id = $c->insert(Model::getTable('Contact'), true, array(''), Entity::SANITIZE);
+                $service->sendEmail();
+                $service->persist();
                 $this->redirect('contact/thanks/2');
             }
         }
-        $this->assign('contact', $c);
-        $this->presenter->assign('contact', $c);
-        $this->presenter->assign('contact_type_id', $_GET['id']);
+        $this->assign('contact', $service->getContact());
+        $this->assign('contact_type_id', $_GET['id']);
         $this->setTemplate('elib://contact.tpl');
     }
 
