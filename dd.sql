a33e050eedb550bc5971f2277d3e360a24b7e28a
Mike Whiting <mike.whiting@eqtr.com>
2022-07-27 18:25:42 +0100

    update dd

--- dd.sql
+++ dd.sql
@@ -27,7 +27,7 @@ FOREIGN KEY (user_profile_id) REFERENCES user_profile(id)) ENGINE=InnoDB;
 
 CREATE TABLE contact(
 id                      INT(11)                 AUTO_INCREMENT PRIMARY KEY,
-user_id         		INT(11)                 NULL,
+user_id                 INT(11)                 NULL,
 message                 TINYINT(1)              NOT NULL DEFAULT 0,
 subject                 VARCHAR(255)            NULL,
 body                    TEXT                    NULL,
@@ -38,16 +38,16 @@ submitted               TIMESTAMP               NULL,
 FOREIGN KEY (user_id) REFERENCES e_user(id)) ENGINE=InnoDB;
 
 CREATE TABLE shipping_address(
-id     			        INT(11)					AUTO_INCREMENT PRIMARY KEY,
-user_id			        INT(11)					NOT NULL,
-first_name		        VARCHAR(128)			NULL,
-last_name		        VARCHAR(128)			NULL,
-address1		        VARCHAR(128)			NULL,
-address2		        VARCHAR(128)			NULL,
-city			        VARCHAR(128)			NULL,
-state			        VARCHAR(128)			NULL,
-zip			            VARCHAR(128)			NULL,
-country			        VARCHAR(128)			NULL,
-default_address		    TINYINT(1)				NOT NULL DEFAULT 0,
+id                      INT(11)                 AUTO_INCREMENT PRIMARY KEY,
+user_id                 INT(11)                 NOT NULL,
+first_name              VARCHAR(128)            NULL,
+last_name               VARCHAR(128)            NULL,
+address1                VARCHAR(128)            NULL,
+address2                VARCHAR(128)            NULL,
+city                    VARCHAR(128)            NULL,
+state                   VARCHAR(128)            NULL,
+zip                     VARCHAR(128)            NULL,
+country                 VARCHAR(128)            NULL,
+default_address         TINYINT(1)              NOT NULL DEFAULT 0,
 FOREIGN KEY (user_id) REFERENCES e_user(id)) ENGINE=InnoDB;
 
