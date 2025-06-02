6f56afbc25765043246d075fe7fb114aeeec934f
Mike Whiting <mail@mikejw.co.uk>
2025-06-02 07:13:16 +0100

    deprecate method

--- src/Empathy/ELib/User/CurrentUser.php
+++ src/Empathy/ELib/User/CurrentUser.php
@@ -93,6 +93,12 @@ class CurrentUser
         return ($this->getUserID() > 0);
     }
 
+    #[Deprecated(message: "no longer standard property", since: "4.0.2")]
+    public function getProfileID()
+    {
+        return $this->u->user_profile_id;
+    }
+
     public function getUser()
     {
         return $this->u;
