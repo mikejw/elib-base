1662ac010b506210d0b3566cca835816718e939f
Mike Whiting <mail@mikejw.co.uk>
2025-06-11 07:13:49 +0000

    turn off memcache compression and set json version of data

--- src/Empathy/ELib/VCache/DriverMemcached.php
+++ src/Empathy/ELib/VCache/DriverMemcached.php
@@ -4,17 +4,20 @@ namespace Empathy\ELib\VCache;
 
 class DriverMemcached extends Driver
 {
-
     private $m;
 
     public function load($h, $p)
     {
         $this->m = new \Memcached();
-        $this->m->setOption(\Memcached::OPT_COMPRESSION, true);
+        $this->m->setOption(\Memcached::OPT_COMPRESSION, false);
         $this->m->setOption(\Memcached::OPT_PREFIX_KEY, 'default');
-
         $this->m->addServer($h, $p);
+    }
 
+    public function set($key, $value, $timeout=0)
+    {
+        $this->m->set($key, $value, $timeout);
+        $this->m->set($key . '_json', json_encode($value), $timeout);
     }
 
     public function get($key)
@@ -22,23 +25,8 @@ class DriverMemcached extends Driver
         return $this->m->get($key);
     }
 
-    public function set($key, $value, $timeout=0)
-    {
-        $this->m->set($key, $value, $timeout);
-
-    }
-
     public function delete($key)
     {
         return $this->m->delete($key);
     }
-
-    /*
-
-      public function init()
-      {
-      echo 'new caching object.. ready to party.';
-      }
-    */
-
 }
