bacc06904406e8790cc92d17e1d382b0cc79d7ab
Mike Whiting <mikejw3@gmail.com>
2026-03-29 12:59:43 +0100

    add rector

---
+++ rector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_84)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    );
+    
